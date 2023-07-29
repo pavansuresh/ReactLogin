e92d7ea4160cd5973c0bda83fcf54683471edb68
Pavan Kumar <pavan.suresh23@gmail.com>
2023-07-29 13:36:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ function App() {
 
   return (
     <>
-    {/* https://www.youtube.com/watch?v=X3qyxo_UTR4&list=PL0Zuz27SZ-6PRCpm9clX0WiBEMB70FWwd&index=2 */}
        <main className="App">
           {/* <Register/> */}
           <Login/>
